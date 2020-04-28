9b8ba7f5cd6bb01c041570a942d653db0bdafb96
Manmohan Singh Bohara <manmohan.bohara@gmail.com>
2020-04-28 15:32:34 +0900

    Update create-iam-console-user.py

--- AutomatingAWS/IAM/create-iam-console-user.py
+++ AutomatingAWS/IAM/create-iam-console-user.py
@@ -1,11 +1,33 @@
 import boto3
 import json, string, random
 
+from botocore.exceptions import ClientError
+
 def lambda_handler(event, context):
     
+    # Creating an boto3 client representing IAM service.
     iam_client = boto3.client('iam')
     
-    user_name = 'sri.bohara'
+    
+    # Recieve group and user name as input
+    
+    group_name = event['GroupName']
+    user_name = event['UserName']
+    sender_email = event['SenderEmail']
+    reciever_email = event['ReceiverEmail']
+    account_id = event['AccountId']
+    policy_name = event['PolicyName']
+    
+    try:
+        group_response = iam_client.create_group(
+            GroupName= group_name)
+    except ClientError as error:
+        if error.response['Error']['Code'] == 'EntityAlreadyExists':
+            print('Group already exists')
+        else:
+            print('Unexpected error occured while creating group... exiting from here', error)
+            return 'User could not be create', error
+        
     try:
         user = iam_client.create_user(
             UserName=user_name,
@@ -16,8 +38,13 @@ def lambda_handler(event, context):
                 }
                 ]
             )
-    except:
-        pass
+    except ClientError as error:
+        if error.response['Error']['Code'] == 'EntityAlreadyExists':
+            print('User already exists')
+            return 'User already exists'
+        else:
+            print('Unexpected error occured while creating user.... exiting from here', error)
+            return 'User could not be create', error 
         
     policy_json = {
                 "Version": "2012-10-17",
@@ -31,8 +58,6 @@ def lambda_handler(event, context):
             }
     
     policy_arn = ''
-    account_id = '827178005985'
-    policy_name = 'ec2-full-access'
     
     try:
         policy = iam_client.create_policy(
@@ -40,16 +65,27 @@ def lambda_handler(event, context):
                 PolicyDocument= json.dumps(policy_json)
             )
         policy_arn = policy['Policy']['Arn']
-    except:
-        policy_arn = 'arn:aws:iam::'+account_id+':policy/'+policy_name
+    except ClientError as error:
+        if error.response['Error']['Code'] == 'EntityAlreadyExists':
+            print('Policy already exists... Hence retreiving policy arn')
+            policy_arn = 'arn:aws:iam::'+account_id+':policy/'+policy_name
+        else:
+            print('Unexpected error occured while creating policy... hence cleaning up', error)
+            iam_client.delete_user( UserName= user_name)
+            return 'User could not be create', error
     
+    try:
+        response = iam_client.attach_user_policy(
+                UserName= user_name,
+                PolicyArn= policy_arn
+            )
+    except ClientError as error:
+        print('Unexpected error occured while attaching policy... hence cleaning up', error)
+        iam_client.delete_user( UserName= user_name)
+        return 'User could not be create', error
+            
     
-    response = iam_client.attach_user_policy(
-            UserName= user_name,
-            PolicyArn= policy_arn
-        )
-        
-    password = randomString()
+    password = random_string()
         
     try:
         login_profile = iam_client.create_login_profile(
@@ -57,14 +93,48 @@ def lambda_handler(event, context):
                 Password = password,
                 PasswordResetRequired = True
             )
-    except:
-        pass
+    except ClientError as error:
+        if error.response['Error']['Code'] == 'EntityAlreadyExists':
+            print('login profile already exists')
+        else:
+            print('Unexpected error occured while creating login profile... hence cleaning up', error)
+            return 'User could not be create', error
     
     print('User with UserName:{0} got created successfully'.format(user_name))
     
-    return user_name, password
+    # Add user to group
     
-
-def randomString(stringLength=12):
+    add_user_to_group_res = iam_client.add_user_to_group(
+        GroupName= group_name,
+        UserName= user_name
+        )
+    
+    # Now user got created... Sending its details via email
+    
+    ses_client = boto3.client('ses')
+    
+    ses_res = ses_client.send_email(
+        Source= sender_email,
+        Destination= {
+            'ToAddresses': [
+                reciever_email
+                ]
+        },
+        Message= {
+            'Subject': {
+                'Data': 'You IAM user deatils'
+            },
+            'Body': {
+                'Text': {
+                    'Data': 'User name is : "{0}" \nOne time password is : "{1}"'.format(user_name, password)
+                }
+            }
+        }
+        )
+    
+    return 'User with UserName:{0} got created successfully'.format(user_name)
+    
+# This function generates random string
+def random_string(stringLength=12):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
